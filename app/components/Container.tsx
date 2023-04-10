5f06771b4b94a9f7a6dadadadab5bb74f7162680
pheaem <pheaem@puc.edu.kh>
2023-04-10 09:53:10 +0700

    first

---
+++ app/components/Container.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface ContainerProps {
+    children: React.ReactNode
+}
+
+const Container = () => {
+    return ( 
+        <div>Container</div>
+     );
+}
+ 
+export default Container;
